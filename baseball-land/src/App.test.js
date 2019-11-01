93eec75e333bb80f1c61b60d2cb424f8718c6532
Eric Foster <elitheroyalone@gmail.comgit config --global user.name eric-fostergit config --global user.email elitheroyalone@gmail.com>
2019-10-31 22:20:41 -0500

    Testing

--- baseball-land/src/App.test.js
+++ baseball-land/src/App.test.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+
+test('renders dashboard component', () => {
+  const wrapper = rtl.render(<Dashboard/>)
+  const element = wrapper.getByText(/hit/i)
+  expect(element).toBeInTheDocument()
+}) 
+
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
